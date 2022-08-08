cbd310e1c836bcb9f673dde692d1389a9bcb6695
FELIPEJ.COMPASSO@fleetcor.com.br <FELIPEJ.COMPASSO@fleetcor.com.br>
2022-08-08 15:40:46 -0300

    adicionando ejs

---
+++ src/index.ts
@@ -0,0 +1,85 @@
+import * as https from "https"
+import axios, { AxiosRequestConfig } from "axios"
+import fs from "fs"
+import path from "path"
+import Express from "express"
+
+const app = Express()
+
+app.set('view engine', 'ejs')
+app.set('views', 'src/view')
+
+app.get('/', (req, res) => {
+  
+  if(process.env.NODE_ENV !== 'PROD'){
+    require("dotenv").config()
+  }
+
+  //carregando o certificado
+  const cert = fs.readFileSync(
+    path.resolve(__dirname,`../hml/${process.env.CERTIFICATE}`) // o path.resolve retorna o caminho certinho de onde está o arquivo
+  )
+
+  const agent = new https.Agent({
+    pfx:cert,
+    passphrase:""
+  })
+
+  //transformano as credenciais em base64
+  const creadntials = Buffer.from(`${process.env.CLIENT_ID}:${process.env.CLIENT_SECRET}`)
+  .toString("base64")
+
+  // curl 
+  axios({
+    method:"POST",
+    url:`${process.env.ENDPOINT}/oauth/token`,
+    headers:{
+      Authorization:`Basic ${creadntials}`,
+      'Content-Type':'application/json'
+    },
+    httpsAgent: agent,
+    data: {
+      "grant_type": "client_credentials"
+    }
+  }).then(res => {
+    const acessToken = res.data?.access_token
+    //configurações padrões 
+    const request = axios.create({
+      baseURL:process.env.ENDPOINT,
+      httpsAgent:agent,
+      headers:{
+        "Authorization":`Bearer ${acessToken}`,
+        'Content-Type':'application/json'
+      }
+    })
+
+    // const cob  = `${process.env.ENDPOINT}/v2/cob`
+
+    const data = {
+      "calendario": {
+        "expiracao": 3600
+      },
+      "valor": {
+        "original": "100.00"
+      },
+      "chave": "philipecout@gmail.co",
+      "solicitacaoPagador": "Informe o número ou identificador do pedido."
+    }
+
+    // const config:AxiosRequestConfig = {
+    //   httpsAgent:agent,
+    //   headers:{
+    //     "Authorization":`Bearer ${acessToken}`,
+    //     'Content-Type':'application/json'
+    //   }
+    // }
+    request.post('/v2/cob',data).then( response =>{
+        // res.status('200').json()
+    })
+
+  })
+
+})
+
+app.listen(3333, () => console.log('running 3333'))
+
